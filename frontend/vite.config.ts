b03dd08915eaaff836c14411e57edc88892b1a59
[dyad] <rnlmntz@gmail.com>
2025-09-06 19:14:24 +0800

    [dyad] wrote 1 file(s) + extra files edited outside of Dyad

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,12 +4,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['xterm', 'xterm-addon-fit', 'simple-icons'], // Added 'simple-icons'
+    include: ['xterm', 'xterm-addon-fit', 'simple-icons'],
   },
   build: {
     rollupOptions: {
       // Removed 'xterm' and 'xterm-addon-fit' from external to ensure they are bundled.
       // external: ['xterm', 'xterm-addon-fit'], 
+    },
+    commonjsOptions: { // Add this to help Rollup resolve CommonJS modules
+      include: [/node_modules\/simple-icons/],
     },
   },
 });
